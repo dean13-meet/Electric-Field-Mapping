b471206ed8b5c5cb6948807798306292ec53077a
William Lee <masteratsoccer@gmail.com>
2014-05-11 20:29:33 -0700

    Added @authors

--- src/src/Main/Main.java
+++ src/src/Main/Main.java
@@ -1,6 +1,8 @@
+/**
+ * @Authors William Lee, Dean Leitersdorf, Ophir Sneh, and Lilia Tang
+ */
 package src.Main;
 public class Main {
-
 	public static void main(String[] args) { 
 		// Bring up a JFrame with squares to represent the cells
 		//Don't need a full on declaration (Program p = new Program), updated it -William Lee
